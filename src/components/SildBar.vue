a1e16623b059d16cc44f89d318163e173909b500
BBgego <1348281410@qq.com>
2019-05-20 16:11:38 +0800

    轮播图功能实现

--- src/components/SildBar.vue
+++ src/components/SildBar.vue
@@ -1,52 +1,57 @@
 <template>
   <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">
-        <img src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1262887.jpg">
-      </div>
-      <div class="swiper-slide">
-        <img src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1262887.jpg">
-      </div>
-      <div class="swiper-slide">
-        <img src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1262887.jpg">
-      </div>
-      <div class="swiper-slide">
-        <img src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1262887.jpg">
-      </div>
-      <div class="swiper-slide">
-        <img src="http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1262887.jpg">
+      <div class="swiper-slide" v-for="item in slider_list" :key="item.url">
+        <a :href="item.url">
+          <img :src="item.silder_img_url" :alt="item.silder_img_name">
+        </a>
       </div>
     </div>
     <!-- Add Pagination -->
     <div class="swiper-pagination"></div>
-    <!-- Add Arrows -->
-    <!-- <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "SildBar",
-  // mounted: function() {
-  //   var oHead = document.getElementsByTagName("HEAD").item(0);
-  //   var oScript = document.createElement("script");
-  //   oScript.type = "text/javascript";
-  //   oScript.src = "";
-  //   oHead.appendChild(oScript);
-  // }
+  data: function() {
+    return {
+      slider_list: []
+    };
+  },
   mounted: function() {
-    var swiper = new Swiper(".swiper-container", {
-      pagination: ".swiper-pagination",
-      // nextButton: ".swiper-button-next",
-      // prevButton: ".swiper-button-prev",
-      paginationClickable: true,
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: 2500,
-      autoplayDisableOnInteraction: false,
-      loop: true
-    });
+    this.loadData();
+    this.swiper();
+  },
+  methods: {
+    loadData: function() {
+      this.$ajax
+        .get("http://127.0.0.1:8000/silder")
+        .then(response => {
+          if (response.status === 200) {
+            this.slider_list = response.data;
+            this.$nextTick(() => {
+              // 下一个UI帧再初始化swiper
+              this.swiper();
+            });
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    swiper: function() {
+      var swiper = new Swiper(".swiper-container", {
+        pagination: ".swiper-pagination",
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+        loop: true
+      });
+    }
   }
 };
 </script>
@@ -65,7 +70,6 @@ export default {
   text-align: center;
   font-size: px2rem(18);
   background: #fff;
-  /* Center slide text vertically */
   display: -webkit-box;
   display: -ms-flexbox;
   display: -webkit-flex;
@@ -79,18 +83,17 @@ export default {
   -webkit-align-items: center;
   align-items: center;
 }
-.swiper-slide img{
+.swiper-slide img {
   width: 100%;
 }
 
 .swiper-pagination-bullet {
   height: px2rem(16);
   width: px2rem(16);
-  background-color:#fff;
+  background-color: #fff;
 }
 
-.swiper-pagination-bullet-active{
+.swiper-pagination-bullet-active {
   background-color: #31c27c;
 }
 </style>
-
